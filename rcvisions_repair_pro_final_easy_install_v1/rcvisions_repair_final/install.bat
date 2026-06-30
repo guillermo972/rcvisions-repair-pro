c8645f34663b5672002c69845d2ee54c5c4a3194
guillermo972 <guillermo@rcvisions.com>
2026-06-29 18:17:06 -0700

    Delete rcvisions_repair_pro_final_easy_install_v1/rcvisions_repair_final directory

--- rcvisions_repair_pro_final_easy_install_v1/rcvisions_repair_final/install.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd /d "%~dp0"
-echo Installing RCvisions Repair Pro email support...
-npm install
-pause
--- rcvisions_repair_pro_final_easy_install_v1/rcvisions_repair_final/start.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd /d "%~dp0"
-start http://localhost:3000/staff.html
-npm start
-pause
